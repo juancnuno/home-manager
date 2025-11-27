cc2f87240ee6e56d928cb06064e3a04449ee4376
Juan C Nuno <juancnuno@gmail.com>
2025-11-27 06:20:51 -0800

    Use identifiers instead of string literals

--- vscode.nix
+++ vscode.nix
@@ -3,21 +3,21 @@
     enable = true;
 
     profiles.default.userSettings = {
-      "breadcrumbs.enabled" = false;
-      "chat.disableAIFeatures" = true;
-      "editor.cursorBlinking" = "solid";
-      "editor.minimap.enabled" = false;
+      breadcrumbs.enabled = false;
+      chat.disableAIFeatures = true;
+      editor.cursorBlinking = "solid";
+      editor.minimap.enabled = false;
 
-      "editor.rulers" = [
+      editor.rulers = [
         100
       ];
 
-      "editor.stickyScroll.enabled" = false;
-      "files.trimTrailingWhitespace" = true;
-      "jdk.telemetry.enabled" = true;
-      "window.menuStyle" = "custom";
-      "window.titleBarStyle" = "native";
-      "workbench.editor.enablePreview" = false;
+      editor.stickyScroll.enabled = false;
+      files.trimTrailingWhitespace = true;
+      jdk.telemetry.enabled = true;
+      window.menuStyle = "custom";
+      window.titleBarStyle = "native";
+      workbench.editor.enablePreview = false;
     };
   };
 }
