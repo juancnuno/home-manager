7a6e0264a6af3ba3109f6726adde1fafd753bca1
Juan C Nuno <juancnuno@gmail.com>
2025-12-06 11:57:42 -0800

    Use xdg.desktopEntries."com.google.Chrome" instead

--- google-chrome.nix
+++ google-chrome.nix
@@ -1,24 +1,18 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  home.packages =
-    let
-      com-google-chrome-desktop = pkgs.runCommand "com.google.Chrome.desktop" { } ''
-        mkdir -p $out/share/applications
-        cd $out/share/applications
-        cp ${pkgs.google-chrome}/share/applications/com.google.Chrome.desktop .
-        sed -i /NoDisplay=true/d com.google.Chrome.desktop
-      '';
+  home.packages = [ pkgs.google-chrome ];
 
-      hi-prio-com-google-chrome-desktop = lib.hiPrio com-google-chrome-desktop;
-    in
-    [
-      hi-prio-com-google-chrome-desktop
-      pkgs.google-chrome
-    ];
+  xdg.desktopEntries = {
+    "com.google.Chrome" = {
+      exec = "${pkgs.google-chrome}/bin/google-chrome-stable --class=com.google.Chrome";
+      icon = "google-chrome";
+      name = "Google Chrome";
+    };
 
-  xdg.desktopEntries.google-chrome = {
-    name = "";
-    settings.Hidden = "true";
+    google-chrome = {
+      name = "";
+      settings.Hidden = "true";
+    };
   };
 }
